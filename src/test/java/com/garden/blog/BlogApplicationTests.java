be497eab0e7a658b8ac8e5f4635612ba53dcc705
garden-jun <jeongwonjun0718@gmail.com>
2021-05-11 19:38:30 +0900

    환경세팅 완료

---
+++ src/test/java/com/garden/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garden.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
